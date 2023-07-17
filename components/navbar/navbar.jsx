aa910713496bc992acd23f189b4da8f2a3e8ff34
kesavan <kesavan@mericaninc.com>
2023-07-17 11:16:04 +0530

    add navbar and install react-icons

---
+++ components/navbar/navbar.jsx
@@ -0,0 +1,21 @@
+import { FaMapLocationDot } from "react-icons/fa6";
+import { FaSearch, FaRegHeart } from "react-icons/fa";
+const Navbar = () => {
+  return (
+    <nav className="p-5 bg-slate-300 text-[30px] text-slate-700 shadow-xl">
+      <ul className="flex flex-col gap-10">
+        <li className="bg-slate-200 p-2 rounded-md hover:bg-slate-400 ">
+          <FaMapLocationDot />
+        </li>
+        <li className="bg-slate-200 p-2 rounded-md hover:bg-slate-400 ">
+          <FaSearch />
+        </li>
+        <li className="bg-slate-200 p-2 rounded-md hover:bg-slate-400 ">
+          <FaRegHeart />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
